4e82b22ca2c25e1899594f2f018c3e7ad75e6f3a
jillytot <jill@aylogames.com>
2013-11-25 15:01:02 -0800

    Rocks now change graphics as they are destroyed. Added "Sparks" particles that play when you hit rock with a pickaxe

--- Assets/Scripts/rockBehavior.cs
+++ Assets/Scripts/rockBehavior.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class rockBehavior : MonoBehaviour {
 
-	public int rockHP = 3;
+	public int rockHP = 3; //HP of rock
+	bool killChild; //Used to generate new image depending on HP or rock.
+	public GameObject[] rockGraphics; //Different children which can spawn to show the state of the rock visually. 
+	public GameObject sparks;
 
 	// Use this for initialization
 	void Start () {
+
+		renderer.enabled = false; //toggle to make default rock object visible during gameplay.
+		killChild = false;
+		generateChild ();
 	
 	}
 	
@@ -20,12 +27,43 @@ public class rockBehavior : MonoBehaviour {
 	
 	}
 
+	void destroyChild () {
+
+	foreach (Transform kid in this.transform) {
+			//print ("kid: " + kid.name);
+			Destroy (kid.gameObject);
+		}
+	}
+
+	//Controlls rules for taking damage
 	void OnTriggerEnter (Collider other) {
 
 		var imHit = other.GetComponent<Adventurer>();
 
 		if (Adventurer.pickAxe == true) {
 			rockHP -= 1;
+			GameObject spawnSparks = (GameObject)Instantiate(sparks,transform.position, transform.rotation);
+			destroyChild ();
+			generateChild ();
+
 		}
 	}
+
+	//Genrates new graphics based on rockHP
+	void generateChild () {
+		switch(rockHP) {
+		case 3:
+			(Instantiate (rockGraphics[2], transform.position, transform.rotation) as GameObject).transform.parent = this.transform;
+			break;
+		case 2:
+			(Instantiate (rockGraphics[1], transform.position, transform.rotation) as GameObject).transform.parent = this.transform;
+			break;
+		case 1:
+			(Instantiate (rockGraphics[0], transform.position, transform.rotation) as GameObject).transform.parent = this.transform;
+			break;
+		default:
+			break;
+		}
+	}
+
 }
---
+++ Assets/playerCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerCollider : MonoBehaviour {
+
+	public GameObject sparks;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+/*	void OnTriggerEnter (Collider other) {
+		
+		var rockHit = other.GetComponent<rockBehavior>();
+		
+		if (Adventurer.pickAxe == true) {
+
+			GameObject spawnSparks = (GameObject)Instantiate(sparks,transform.position, transform.rotation);
+
+			
+		}
+	} */
+}
