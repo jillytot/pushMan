687b3a099c024bbb41b8205be1d54664a331e729
jillytot <jill@aylogames.com>
2013-12-06 00:59:13 -0800

    Backing up before reverting….

--- Assets/Scripts/wallColliderBehavior.cs
+++ Assets/Scripts/wallColliderBehavior.cs
@@ -3,12 +3,33 @@ using System.Collections;
 
 public class wallColliderBehavior : MonoBehaviour {
 
+	Vector3 rayDirection;
+	bool iShouldntBeHere = false;
+
 	// Use this for initialization
 	void Start () {
+
+		rayDirection = transform.TransformDirection(Vector3.up) * 1.0f;
 
 		//comment this out to make the wall collider visible in game.
 		renderer.enabled = false;
 	
+	}
+
+	void Update () { 
+
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, rayDirection, out hit, 1.0f)) {
+			
+			iShouldntBeHere = hit.collider.CompareTag("Tile");
+			
+			if (iShouldntBeHere) {
+
+				
+				Debug.Log("I should not be here");
+			}
+		}
 	}
 	
 	void OnTriggerEnter (Collider other) {
